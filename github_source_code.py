01cf35a9867b1e61b1d17fd26925d2833a328be9
jagadabhibalaji <141623034+jagadabhibalaji@users.noreply.github.com>
2023-08-07 15:00:46 +0530

    Add files via upload
    Hello program

---
+++ github_source_code.py
@@ -0,0 +1,8 @@
+
+def say_hello(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    greeting = say_hello(user_name)
+    print(greeting)
