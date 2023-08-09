40fcd01f3c88c083538b71fcea4ce72ae51f79ed
jagadabhibalaji <141623034+jagadabhibalaji@users.noreply.github.com>
2023-08-09 12:30:42 +0530

    Update github_source_code.py

--- github_source_code.py
+++ github_source_code.py
@@ -1,7 +1,12 @@
-def say_hello(name):
-    return f"Hello, {name}!"
+# def say_hello(name):
+#     return f"Hello, {name}!"
 
-if __name__ == "__main__":
-    user_name = input("Enter your name: ")
-    greeting = say_hello(user_name)
-    print(greeting)
+# if __name__ == "__main__":
+#     user_name = input("Enter your name: ")
+#     greeting = say_hello(user_name)
+#     print(greeting)
+
+
+l1=[1,2,3,7,9,10]
+evens=[[num] for num in l1 if num%2==0]
+print("Even numbers in the list :",evens)
